60e5e3fe9b5c83d1b23e00c1520c26434096db4d
Phạm Bá Trung Thành <phambatrungthanh@gmail.com>
2019-05-20 16:28:27 +0700

    Update depedencies to add tailwindcss

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  presets: [['@vue/app', { useBuiltIns: 'entry' }]]
+  presets: [
+    [
+      '@vue/app',
+      {
+        useBuiltIns: 'entry'
+      }
+    ],
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ]
+  ]
 }
