8cd889b3c50c08c0e13f3a946c4ee4e43ddfddf2
Phạm Bá Trung Thành <phambatrungthanh@gmail.com>
2019-06-17 17:11:10 +0700

    new helpers

--- src/misc/helpers.js
+++ src/misc/helpers.js
@@ -16,3 +16,20 @@ export const createNewUserFromFirebaseAuthUser = async firebaseAuthUser => {
 
   return await userDb.create(user, firebaseAuthUser.uid)
 }
+
+export const localStorageAvailable = () => {
+  const test = 'test'
+  try {
+    localStorage.setItem(test, test)
+    localStorage.removeItem(test)
+    return true
+  } catch (e) {
+    return false
+  }
+}
+
+export function getCurrentGeolocation(options = {}) {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject, options)
+  })
+}
