aff5c501c59e398dccc5054c49532888f2a1ef68
Phạm Bá Trung Thành <phambatrungthanh@gmail.com>
2019-06-17 17:11:32 +0700

    Damn fix on google maps

--- src/views/GoogleMaps.vue
+++ src/views/GoogleMaps.vue
@@ -3,9 +3,8 @@
 </template>
 <script>
 import GoogleMapsInit from '@/misc/google-maps-init'
-import { mapGetters, mapActions } from 'vuex'
+import { mapGetters, mapActions, mapState } from 'vuex'
 import debounce from 'lodash/debounce'
-import { makeRe } from 'minimatch'
 
 const PINS_SPIN_TIME = 20
 export default {
@@ -23,52 +22,50 @@ export default {
   },
   computed: {
     ...mapGetters('map', ['defaultMapStyle']),
-    ...mapGetters('dishes', ['pins'])
+    ...mapState('dishes', ['pinsOnMap'])
   },
   watch: {
-    pins: function(newPins, oldPins) {
-      // make a clone of old pins
-      const pinsToRemove = this.pins
-
-      // loop through new pins
-      const pinsToAdd = []
-      for (const newPin of newPins) {
-        // if the old pin pool is gone, skip the check
-        if (pinsToRemove.length > 0) {
-          const index = pinsToRemove.findIndex(pin => pin.dish_id === newPin.id)
-          if (index === -1) {
-            // this pins is new
-            pinsToAdd.push(newPin)
-            continue
-          }
-          // else, this pin is already on the map, remove from old pin pool
-          pinsToRemove.splice(index, 1)
-        }
-      }
-
-      this.removePins(pinsToRemove)
-      const markers = []
-      for (const pin of pinsToAdd) {
-        // make new custom pin
-        const marker = new this.googleMapApi.maps.Marker({
-          position: new this.googleMapApi.maps.LatLng(
-            pin._geoloc.lat,
-            pin._geoloc.lng
-          ),
-          title: pin.name,
-          label: pin.name,
-          map: this.map,
-          icon: {
-            url: `https://angi.phambatrungthanh.com/storage/${pin.image}`,
-            scaledSize: new this.googleMapApi.maps.Size(32, 32)
-          },
-          dish_id: pin.id
-        })
-        markers.push(marker)
-      }
-      this.addPins(markers)
-      this.pinsOnMap = markers
-    }
+    // pins: function(newPins, oldPins) {
+    //   // make a clone of old pins
+    //   const pinsToRemove = this.pins
+    //   // loop through new pins
+    //   const pinsToAdd = []
+    //   for (const newPin of newPins) {
+    //     // if the old pin pool is gone, skip the check
+    //     if (pinsToRemove.length > 0) {
+    //       const index = pinsToRemove.findIndex(pin => pin.dish_id === newPin.id)
+    //       if (index === -1) {
+    //         // this pins is new
+    //         pinsToAdd.push(newPin)
+    //         continue
+    //       }
+    //       // else, this pin is already on the map, remove from old pin pool
+    //       pinsToRemove.splice(index, 1)
+    //     }
+    //   }
+    //   this.removePins(pinsToRemove)
+    //   const markers = []
+    //   for (const pin of pinsToAdd) {
+    //     // make new custom pin
+    //     const marker = new this.googleMapApi.maps.Marker({
+    //       position: new this.googleMapApi.maps.LatLng(
+    //         pin._geoloc.lat,
+    //         pin._geoloc.lng
+    //       ),
+    //       title: pin.name,
+    //       label: pin.name,
+    //       map: this.map,
+    //       icon: {
+    //         url: `https://angi.phambatrungthanh.com/storage/${pin.image}`,
+    //         scaledSize: new this.googleMapApi.maps.Size(32, 32)
+    //       },
+    //       dish_id: pin.id
+    //     })
+    //     markers.push(marker)
+    //   }
+    //   this.addPins(markers)
+    //   this.pinsOnMap = markers
+    // }
   },
   async mounted() {
     try {
@@ -82,7 +79,6 @@ export default {
   methods: {
     initializeMap() {
       this.map = new this.googleMapApi.maps.Map(this.$refs.googleMap, {
-        center: { lat: 20.9969152, lng: 105.84064 },
         zoom: 17,
         mapTypeId: 'roadmap',
         disableDefaultUI: true,
@@ -104,23 +100,23 @@ export default {
     },
     removePins(pinsToRemove = []) {
       // remove old pins
-      let index = 0
-      for (const pin of pinsToRemove) {
-        setTimeout(function() {
-          pin.setMap(null)
-        }, index * PINS_SPIN_TIME)
-        index++
-      }
+      // let index = 0
+      // for (const pin of pinsToRemove) {
+      //   setTimeout(function() {
+      //     pin.setMap(null)
+      //   }, index * PINS_SPIN_TIME)
+      //   index++
+      // }
     },
     addPins(pinsToAdd) {
       // add new
-      let index = 0
-      for (const pin of pinsToAdd) {
-        setTimeout(function() {
-          pin.setMap(this.map)
-        }, index * PINS_SPIN_TIME)
-        index++
-      }
+      // let index = 0
+      // for (const pin of pinsToAdd) {
+      //   setTimeout(function() {
+      //     pin.setMap(this.map)
+      //   }, index * PINS_SPIN_TIME)
+      //   index++
+      // }
     },
     ...mapActions('map', [
       'setCurrentMapCoordinates',
